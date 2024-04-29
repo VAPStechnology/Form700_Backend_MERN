a724a9e6d5eba7abbb8c07f13aa7351977e5f10c
Pkamal2002 <135655190+Pkamal2002@users.noreply.github.com>
2024-04-29 11:01:22 +0530

    User Controller Completed and write routes in routerJs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -219,10 +219,99 @@ const refreshAccessToken = asyncHandler(async(req,res) =>{
 
 })
 
+//////////////////Update controller starts here////////////////////
+
+///password update section starts here////
+
+const changeCurrentPassword = asyncHandler(async(req,res) => {
+    const {oldPassword, newPassword, confPassword} = req.body
+
+    if(!(newPassword === confPassword)){
+        throw new ApiError(400, "Confirm Password doesn't match")
+    }
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password change successfully"))
+})
+
+/// Current User fetching section starts here///
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res 
+    .status(200)
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
+})
+
+
+/// Update accounts details starts here///////
+
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    if (!fullname || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{fullname, email}
+        },
+        {new: true}
+    ).select("-password")
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+})
+
+/// Update Avatar section starts here///
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is Missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url){
+        throw new ApiError(400, "Error while uploading on avatar")
+
+    }
+    const user =  await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{avatar: avatar.url}
+        },
+        {new: true}
+    ).select("-password")
+
+    return res 
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar updated successfully")
+    )
+
+})
+
 export {
     registerUser,
     loginUser,
     logOutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar
 
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,23 @@
-import { Router} from 'express';
+import { Router } from 'express';
 // import { logOutUser, loginUser, registrUser } from '../controllers/user.controller.js';
-import { registerUser, loginUser, logOutUser, refreshAccessToken } from '../controllers/user.controller.js';
+import { registerUser, 
+    loginUser, 
+    logOutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 const router = new Router();
 
 router.route('/register').post(
     upload.fields([
-        { name: 'avatar', maxCount: 1}
+        { name: 'avatar', maxCount: 1 }
     ]),
     registerUser
-    )
+)
 
 router.route("/login").post(loginUser)
 
@@ -19,6 +26,11 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
 export default router;
 
